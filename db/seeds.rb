3ed7b8b5615326ef0fe0c0712fcb965c1b378890
Sy Rashid <sy.rashid.11@gmail.com>
2020-10-21 09:32:59 +0300

    FEAT: Added seeds and set up geocoder with my Friend model

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,5 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+sy = Friend.create(name:"Sy Rashid", address: "183 Bridge Rd, Melbourne")
+sy.update(name: "Sy Radish")
+sy.update(address: "Kurdela Sk 11, Beyoğlu/Istanbul")
+
+ed = Friend.create(name:"Ed", address: "Galata Tower")
